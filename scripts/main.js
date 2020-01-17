21a4d4c6218892cd844841121fd627a0233ffdd2
Janeseed <tjdus0515@gmail.com>
2020-01-17 22:35:24 +0900

    The basic structure files of website is added. The layout is considered only for mobile interface.

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/* When touchscreen user click on the main button(of menu bar)
+toggle between hiding and showing the dropdown content
+reference: https://www.w3schools.com/howto/howto_js_dropdown.asp
+ */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+
+}
